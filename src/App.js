414e16840081f34c09f2572bfc3bff9ac1790961
Jon Castillo <jcastillo@evidon.com>
2018-06-12 16:17:58 -0400

    create contats form

--- src/App.js
+++ src/App.js
@@ -23,13 +23,27 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then((contact) => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
           <ListContacts contacts={this.state.contacts}  onDeleteContact={this.removeContact} />
         )}/>
-        <Route path="/create" component={CreateContact} />
+        <Route path="/create" render={({ history }) => (
+          <CreateContact 
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}/>
+        )} />
       </div>
     );
   }
